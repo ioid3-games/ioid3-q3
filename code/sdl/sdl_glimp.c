78643d1f6f6b527ef997b49d2ac0690e61996bc0
ToKu <toku@gmail.com>
2016-12-10 12:57:22 +0100

    ioquake3 -> 3129.

--- code/sdl/sdl_glimp.c
+++ code/sdl/sdl_glimp.c
@@ -519,22 +519,6 @@ static qboolean GLimp_StartDriverAndSetMode(int mode, qboolean fullscreen, qbool
 
 /*
 =======================================================================================================================================
-GLimp_HaveExtension
-=======================================================================================================================================
-*/
-static qboolean GLimp_HaveExtension(const char *ext) {
-	const char *ptr = Q_stristr(glConfig.extensions_string, ext);
-
-	if (ptr == NULL) {
-		return qfalse;
-	}
-
-	ptr += strlen(ext);
-	return ((*ptr == ' ') || (*ptr == '\0')); // verify it's complete string.
-}
-
-/*
-=======================================================================================================================================
 GLimp_InitExtensions
 =======================================================================================================================================
 */
@@ -549,7 +533,7 @@ static void GLimp_InitExtensions(void) {
 
 	glConfig.textureCompression = TC_NONE;
 	// GL_EXT_texture_compression_s3tc
-	if (GLimp_HaveExtension("GL_ARB_texture_compression") && GLimp_HaveExtension("GL_EXT_texture_compression_s3tc")) {
+	if (SDL_GL_ExtensionSupported("GL_ARB_texture_compression") && SDL_GL_ExtensionSupported("GL_EXT_texture_compression_s3tc")) {
 		if (r_ext_compressed_textures->value) {
 			glConfig.textureCompression = TC_S3TC_ARB;
 			ri.Printf(PRINT_ALL, "...using GL_EXT_texture_compression_s3tc\n");
@@ -561,7 +545,7 @@ static void GLimp_InitExtensions(void) {
 	}
 	// GL_S3_s3tc ... legacy extension before GL_EXT_texture_compression_s3tc.
 	if (glConfig.textureCompression == TC_NONE) {
-		if (GLimp_HaveExtension("GL_S3_s3tc")) {
+		if (SDL_GL_ExtensionSupported("GL_S3_s3tc")) {
 			if (r_ext_compressed_textures->value) {
 				glConfig.textureCompression = TC_S3TC;
 				ri.Printf(PRINT_ALL, "...using GL_S3_s3tc\n");
@@ -575,7 +559,7 @@ static void GLimp_InitExtensions(void) {
 	// GL_EXT_texture_env_add
 	glConfig.textureEnvAddAvailable = qfalse;
 
-	if (GLimp_HaveExtension("EXT_texture_env_add")) {
+	if (SDL_GL_ExtensionSupported("EXT_texture_env_add")) {
 		if (r_ext_texture_env_add->integer) {
 			glConfig.textureEnvAddAvailable = qtrue;
 			ri.Printf(PRINT_ALL, "...using GL_EXT_texture_env_add\n");
@@ -591,7 +575,7 @@ static void GLimp_InitExtensions(void) {
 	qglActiveTextureARB = NULL;
 	qglClientActiveTextureARB = NULL;
 
-	if (GLimp_HaveExtension("GL_ARB_multitexture")) {
+	if (SDL_GL_ExtensionSupported("GL_ARB_multitexture")) {
 		if (r_ext_multitexture->value) {
 			qglMultiTexCoord2fARB = SDL_GL_GetProcAddress("glMultiTexCoord2fARB");
 			qglActiveTextureARB = SDL_GL_GetProcAddress("glActiveTextureARB");
@@ -618,7 +602,7 @@ static void GLimp_InitExtensions(void) {
 		ri.Printf(PRINT_ALL, "...GL_ARB_multitexture not found\n");
 	}
 	// GL_EXT_compiled_vertex_array
-	if (GLimp_HaveExtension("GL_EXT_compiled_vertex_array")) {
+	if (SDL_GL_ExtensionSupported("GL_EXT_compiled_vertex_array")) {
 		if (r_ext_compiled_vertex_array->value) {
 			ri.Printf(PRINT_ALL, "...using GL_EXT_compiled_vertex_array\n");
 			qglLockArraysEXT = (void (APIENTRY *)(GLint, GLint))SDL_GL_GetProcAddress("glLockArraysEXT");
@@ -636,7 +620,7 @@ static void GLimp_InitExtensions(void) {
 
 	textureFilterAnisotropic = qfalse;
 
-	if (GLimp_HaveExtension("GL_EXT_texture_filter_anisotropic")) {
+	if (SDL_GL_ExtensionSupported("GL_EXT_texture_filter_anisotropic")) {
 		if (r_ext_texture_filter_anisotropic->integer) {
 			qglGetIntegerv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, (GLint *)&maxAnisotropy);
 
