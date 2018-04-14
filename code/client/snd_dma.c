3a198b1b232e03509f642cc182c6351d8e79b3db
ToKu <toku@gmail.com>
2018-04-14 18:17:47 +0200

    ioquake3 -> 3467.

--- code/client/snd_dma.c
+++ code/client/snd_dma.c
@@ -96,7 +96,7 @@ void S_Base_SoundInfo(void) {
 	} else {
 		Com_Printf("%5d stereo\n", dma.channels - 1);
 		Com_Printf("%5d samples\n", dma.samples);
-		Com_Printf("%5d samplebits\n", dma.samplebits);
+		Com_Printf("%5d samplebits (%s)\n", dma.samplebits, dma.isfloat ? "float" : "int");
 		Com_Printf("%5d submission_chunk\n", dma.submission_chunk);
 		Com_Printf("%5d speed\n", dma.speed);
 		Com_Printf("%p dma buffer\n", dma.buffer);
@@ -117,7 +117,7 @@ S_Base_StartCapture
 =======================================================================================================================================
 */
 static void S_Base_StartCapture(void) {
-	// !!! FIXME: write me.
+	SNDDMA_StartCapture();
 }
 
 /*
@@ -126,8 +126,7 @@ S_Base_AvailableCaptureSamples
 =======================================================================================================================================
 */
 static int S_Base_AvailableCaptureSamples(void) {
-	// !!! FIXME: write me.
-	return 0;
+	return SNDDMA_AvailableCaptureSamples();
 }
 
 /*
@@ -136,7 +135,7 @@ S_Base_Capture
 =======================================================================================================================================
 */
 static void S_Base_Capture(int samples, byte *data) {
-	// !!! FIXME: write me.
+	SNDDMA_Capture(samples, data);
 }
 
 /*
@@ -145,7 +144,7 @@ S_Base_StopCapture
 =======================================================================================================================================
 */
 static void S_Base_StopCapture(void) {
-	// !!! FIXME: write me.
+	SNDDMA_StopCapture();
 }
 
 /*
@@ -154,7 +153,7 @@ S_Base_MasterGain
 =======================================================================================================================================
 */
 static void S_Base_MasterGain(float val) {
-	// !!! FIXME: write me.
+	SNDDMA_MasterGain(val);
 }
 #endif
 /*
